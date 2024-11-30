f9671c6c03335a1fab7ba05cf512b7b3f976b47e
Roberto Orazi <robervgg@gmail.com>
2024-11-29 22:46:19 -0300

    add myenv to git ignore and delete env

--- docker/inferencia.py
+++ docker/inferencia.py
@@ -1,17 +1,11 @@
 import pandas as pd
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 MODEL_PATH = "modelo.h5"
 model = load_model(MODEL_PATH)
 
 
 def predict_from_csv(input_csv: str, output_csv: str):
-    """
-    Carga datos desde un archivo CSV, realiza predicciones y guarda los resultados en otro archivo CSV.
-
-    Args: - input_csv (str): Ruta al archivo CSV con los datos de entrada. - output_csv (str): Ruta donde se guardarán
-    los resultados de predicción.
-    """
     input_data = pd.read_csv(input_csv)
 
     predictions = model.predict(input_data)
